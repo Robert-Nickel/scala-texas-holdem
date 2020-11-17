062b4cce0a463fa664313137442733bdb84292f8
Robert Nickel <robert.nickel@aikq.de>
2020-11-17 15:46:28 +0100

    Changed recursive loops

--- src/main/scala/poker/Main.scala
+++ src/main/scala/poker/Main.scala
@@ -1,13 +1,13 @@
 package main.scala.poker
 
-import main.scala.poker.model.{Player, Table}
+import main.scala.poker.Main.{deck, table}
+import main.scala.poker.model.Table
 import poker.{InitHelper, PlayerDSL, PrintHelper, values}
 
 import scala.Console.println
 import scala.annotation.tailrec
-import scala.collection.immutable.HashMap
 import scala.io.StdIn
-import scala.util.{Failure, Random, Success, Try}
+import scala.util.{Failure, Random, Success}
 
 object Main extends App {
   val startingStack = 200
@@ -19,54 +19,68 @@ object Main extends App {
   val table = Table(InitHelper.createPlayers(names, startingStack), deck)
   val validPlayerOptions = Set("fold", "call") // TODO: add raise X and all-in if its implemented
 
-  Dealer.handOutCards(table.players, deck) match {
-    case Failure(f) => println(f.getMessage())
-      System.exit(0)
-    case Success((newPlayers, newDeck)) =>
-      nextMove(Table(newPlayers, newDeck))
-  }
-
   @tailrec
-  def nextMove(table: Table): Table = {
-    // DRAW
-    drawTable(table)
+  def playRound(table: Table): Table = {
+    if (table.players.count(p => p.isInGame()) > 1) {
+      Dealer.handOutCards(table.players, deck) match {
+        case Failure(f) => println(f.getMessage)
+          System.exit(0)
+          table
+        case Success((newPlayers, newDeck)) =>
+          drawTable(table)
+          // The input once per round seems odd -> shouldn't it be ~once per bettingRound
+          val currentPlayer = table.getCurrentPlayer
+          val input = currentPlayer.name match {
+            case "You" => {
+              Some(getValidatedInput())
+            }
+            case _ => None
+          }
+          val newTable = table match {
+            case table if currentPlayer.isInRound() =>
+              // ACT
+              playBettingRound(input, table)
+            case _ =>
+              // SKIP
+              table
+          }
 
-    val currentPlayer = table.getCurrentPlayer
-    val input = currentPlayer.name match {
-      case "You" => {
-        Some(getValidatedInput())
+          // RECURSE
+          if (newTable.currentBettingRound == 4 || newTable.players.count(p => p.isInRound()) == 1) {
+            newTable
+          } else {
+            playRound(newTable.nextPlayer)
+          }
+        //playRound(table.copy(players = newPlayers, deck = newDeck))
       }
-      case _ => None
-    }
-
-    val newTable = table match {
-      case table if currentPlayer.isInRound() =>
-        // ACT
-        safeCurrentPlayerAct(input, table)
-      case _ =>
-        // SKIP
-        table
-    }
-
-    // RECURSE
-    // TODO: isInRound != isInGame
-    if (newTable.players.exists(p => p.isInRound())) {
-      nextMove(newTable.nextPlayer)
     } else {
-      drawTable(newTable)
       println("Game over")
-      newTable
+      table
     }
   }
 
   @tailrec
-  def safeCurrentPlayerAct(input: Option[String], table: Table): Table = {
+  def playBettingRound(input: Option[String], table: Table): Table = {
+    val maxCurrentBet = table.players.map(p => p.currentBet).max
+    if (table.players.exists(p => p.currentBet != maxCurrentBet && p.isInRound())) {
+      playMove(input, table)
+    } else if (table.currentBettingRound < 4) {
+      // TODO: collect current bets into pot
+      val newTable = table.copy(currentBettingRound = table.currentBettingRound + 1)
+      playBettingRound(input, newTable)
+    } else {
+      table
+    }
+  }
+
+  @tailrec
+  def playMove(input: Option[String], table: Table): Table = {
     table.tryCurrentPlayerAct(input, values) match {
       case Success(newTable) => newTable
       case _ => if (input.isEmpty) {
-        safeCurrentPlayerAct(None, table)
+        playMove(None, table)
         // TODO: Try to get this in table class
-      } else safeCurrentPlayerAct(Some(getValidatedInput()), table)
+      } else playMove(Some(getValidatedInput()), table)
     }
   }
 
